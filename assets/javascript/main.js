ef977eb6a6067fae779ba919638d71d9a0c9b584
jmyager <jmyager6@gmail.com>
2018-04-02 11:25:30 -0400

    initial html framework and files

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+// Player inputs name and presses submit
+// Grab player name and input into database as player 1 object
+// Object includes 3 attributes, "name", "wins", "losses"
+// Object is sent to database
+// Players name is displayed on screen as player 1
+// Second input collects player 2 and does the same
+// If both players have been collected, start the game
+// Players are shown their respective rock, paper, scissor choice
+// Player can choose rock, paper, or scissors
+// Players choice is displayed on the screen
+// if player 1 chose rock and player 2 chose scissors, player 1 wins -- go through all variations
+// add a win to whichever player won the game, add a loss to whichever player lost
+// If browser refreshes, that player is removed from the game and database
